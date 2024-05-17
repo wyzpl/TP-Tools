bd952a38c49a49e0fe6698e949a4f9588fa102ae
wyz <wangyuzhou@richerlink.com>
2024-05-17 18:12:55 +0800

    增加导航栏选中效果

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,30 +1,44 @@
+"use client";
 import Link from "next/link";
+import { useState } from "react";
 import HeaderMenuList from "./HeaderMenuList";
 
 export default function Header() {
-  const menuList = [
+  const [menuList, setMenuList] = useState([
     {
       name: "开发导航",
       href: "/nav",
+      isActive: false,
     },
     {
       name: "工具箱",
       href: "/tools",
+      isActive: false,
     },
     {
       name: "博客",
       href: "/blog",
+      isActive: false,
     },
     {
       name: "社区",
       href: "#",
+      isActive: false,
     },
     {
       name: "案例",
       href: "#",
+      isActive: false,
     },
-  ];
+  ]);
 
+  /* 切换导航栏选中效果 */
+  const handleMenuClick = (i) => {
+    const newMenuList = [...menuList];
+    newMenuList.map((menu) => (menu.isActive = false));
+    newMenuList[i].isActive = true;
+    setMenuList(newMenuList);
+  };
   return (
     <>
       <header className="w-full h-16 px-6 flex items-center justify-between sticky top-0 bg-white bg-opacity-80 backdrop-saturate-100 backdrop-blur-md lg:border-b lg\:border-slate-900\/10 lg\:border-b">
@@ -33,7 +47,7 @@ export default function Header() {
             <h1 className="text-2xl font-bold w-60">TREEPLANTING</h1>
           </Link>
           <nav>
-            <HeaderMenuList menuItems={menuList} />
+            <HeaderMenuList menuItems={menuList} onMenuClick={handleMenuClick} />
           </nav>
         </div>
         <div className="flex items-center lg:gap-x-6">
--- components/layout/HeaderMenuList.jsx
+++ components/layout/HeaderMenuList.jsx
@@ -1,12 +1,17 @@
+"use client";
 import Link from "next/link";
+import { useState } from "react";
+import classNames from "classnames";
 
-export default function HeaderMenuList({ menuItems }) {
+export default function HeaderMenuList({ menuItems, onMenuClick }) {
   return (
     <ul className="flex lg:gap-x-12">
       {menuItems.map((item, index) => (
-        <li key={index}>
+        <li key={index} onClick={() => onMenuClick(index)}>
           <Link href={item.href}>
-            <div className="text-sm font-semibold leading-6 text-gray-500">{item.name}</div>
+            <div className={classNames({ "text-gray-900": item.isActive }, "text-sm font-semibold leading-6 text-gray-500")}>
+              {item.name}
+            </div>
           </Link>
         </li>
       ))}
