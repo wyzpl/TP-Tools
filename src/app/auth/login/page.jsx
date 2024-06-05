88f0f6ad5bc00e637e6ea1d5a8a77ec102e93880
wyz <wangyuzhou@richerlink.com>
2024-06-05 17:19:31 +0800

    update

---
+++ src/app/auth/login/page.jsx
@@ -0,0 +1,91 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import { Button, Checkbox, Form, Input } from "antd";
+
+export default function LoginPage() {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+  return (
+    <>
+      <main className="min-h-screen flex flex-col justify-center  text-center">
+        <div className="flex items-center">
+          <div className="bg-gray-400 h-screen flex flex-col justify-center">
+            <Image width={500} height={500} src="/logo.png" alt="logo" />
+          </div>
+          <Form
+            name="basic"
+            labelCol={{
+              span: 8,
+            }}
+            wrapperCol={{
+              span: 16,
+            }}
+            style={{
+              maxWidth: 600,
+              margin: "0 auto",
+            }}
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
+            <Form.Item
+              label="Username"
+              name="username"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your username!",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your password!",
+                },
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
+
+            <Form.Item
+              name="remember"
+              valuePropName="checked"
+              wrapperCol={{
+                offset: 8,
+                span: 16,
+              }}
+            >
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <Form.Item
+              wrapperCol={{
+                offset: 8,
+                span: 16,
+              }}
+            >
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </main>
+    </>
+  );
+}
