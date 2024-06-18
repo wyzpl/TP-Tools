ad8d05dca8b9e587872b04ff176b6fef4bc755f6
wyz <wangyuzhou@richerlink.com>
2024-06-18 14:32:11 +0800

    update

--- src/app/(basicLayout)/tools/page.jsx
+++ src/app/(basicLayout)/tools/page.jsx
@@ -1,7 +1,19 @@
 import Layout from "@/components/layout";
-
+import React from "react";
+import { Input, Button } from "antd";
 const Home = () => {
-  return <h1>Tools Page!!!!!</h1>;
+  return (
+    <>
+      <main className="flex justify-center items-center">
+        <div className="bg-slate-300 w-96 h-96 mt-16">
+          <form action="/">
+            <Input size="large" placeholder="Basic usage" />
+            <Button type="primary">Submit</Button>
+          </form>
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default Home;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,7 +7,7 @@ const Home = () => {
   const el = useRef(null);
   useEffect(() => {
     const typed = new Typed(el.current, {
-      strings: ["每日瞎忙活，练练代码，学习新技术，接触一些新的领域，不断的在此项目中进行扩展&nbsp;"],
+      strings: ["基于Next.js + TailWind + OpenAI&nbsp;边学边练&nbsp;不断开发&nbsp;不断优化&nbsp;"],
       typeSpeed: 100,
       backSpeed: 0,
       cursorChar: "_",
@@ -36,8 +36,8 @@ const Home = () => {
       </div>
       <div className="flex flex-col justify-center text-start md:text-center mx-6">
         <section className="md:flex md:justify-center">
-          <h2 className="text-4xl lg:text-5xl font-extrabold">植树 DEV , &nbsp;</h2>
-          <h2 className="text-4xl lg:text-5xl mt-2 md:mt-0"> 随便写写 纯瞎忙活</h2>
+          <h2 className="text-4xl lg:text-5xl font-extrabold">植树计划 , &nbsp;</h2>
+          <h2 className="text-4xl lg:text-5xl mt-2 md:mt-0"> 积土成山 积水成渊</h2>
         </section>
         <p className="leading-7 mt-6 text-lg">
           <span ref={el} aria-hidden="true"></span>
@@ -65,7 +65,46 @@ const Home = () => {
             >
               <path d="M4 14a1 1 0 0 1-.78-1.63l9.9-10.2a.5.5 0 0 1 .86.46l-1.92 6.02A1 1 0 0 0 13 10h7a1 1 0 0 1 .78 1.63l-9.9 10.2a.5.5 0 0 1-.86-.46l1.92-6.02A1 1 0 0 0 11 14z"></path>
             </svg>
-            <Link href="/nav"> &nbsp;开始使用</Link>
+            <Link href="/"> &nbsp;敬请期待</Link>
+          </button>
+          <button
+            className="inline-flex items-center justify-center whitespace-nowrap font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-black text-white hover:bg-black/90 hover:scale-105 h-11 rounded-md px-8 text-lg"
+            type="button"
+            aria-haspopup="dialog"
+            aria-expanded="false"
+            aria-controls="radix-:R1qula:"
+            data-state="closed"
+          >
+            <svg width="24" strokeWidth="2" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000">
+              <path
+                d="M5 19.5V5C5 3.89543 5.89543 3 7 3H18.4C18.7314 3 19 3.26863 19 3.6V21"
+                stroke="#FFF"
+                strokeWidth="2"
+                strokeLinecap="round"
+              ></path>
+              <path d="M9 7L15 7" stroke="#FFF" strokeWidth="2" strokeLinecap="round"></path>
+              <path d="M6.5 15L19 15" stroke="#FFF" strokeWidth="2" strokeLinecap="round"></path>
+              <path d="M6.5 18L19 18" stroke="#FFF" strokeWidth="2" strokeLinecap="round"></path>
+              <path d="M6.5 21L19 21" stroke="#FFF" strokeWidth="2" strokeLinecap="round"></path>
+              <path
+                d="M6.5 18C5.5 18 5 17.3284 5 16.5C5 15.6716 5.5 15 6.5 15"
+                stroke="#FFF"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              ></path>
+              <path
+                d="M6.5 21C5.5 21 5 20.3284 5 19.5C5 18.6716 5.5 18 6.5 18"
+                stroke="#FFF"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              ></path>
+            </svg>
+            <Link href="https://blog.zhishujihua.cn/" target="_blank">
+              {" "}
+              &nbsp;博客
+            </Link>
           </button>
         </section>
       </div>
